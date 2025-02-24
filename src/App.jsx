c9f7ca18f75a49d75f2faa09bd9448770c712496
ankitr1845 <ankitr1845@gmail.com>
2025-02-24 19:42:00 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,51 @@
+import React from 'react'
+import "./App.css"
+import AppLayout from './components/Layout/AppLayout'
+
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+
+import { Home } from './pages/Home'
+import { About } from './pages/About'
+import { Country } from './pages/Country'
+import { Contact } from './pages/Contact'
+import ErrorPage from './pages/ErrorPage'
+import { CountryDetails } from './components/Layout/CountryDetails'
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    errorElement: <ErrorPage />,
+    element: <AppLayout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "about",
+        element: <About />,
+      },
+      {
+        path: "country/:id",
+        element: <CountryDetails />,
+      },
+      {
+        path: "country",
+        element: <Country />,
+      },
+      {
+        path: "contact",
+        element: <Contact />,
+      },
+    ]
+  }
+  
+
+])
+
+const App = () => {
+  return <RouterProvider router = {router}></RouterProvider>
+}
+
+export default App
