53240db6023c81b9b4b57138eaca361c6762e6b9
ankitr1845 <ankitr1845@gmail.com>
2025-02-24 19:48:50 +0530

    Add files via upload

---
+++ src/components/Layout/AppLayout.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Footer from '../UI/Footer'
+import Header from '../UI/Header'
+import { Outlet } from 'react-router-dom'
+
+const AppLayout = () => {
+  return (
+    <>
+        <Header />
+        <Outlet />
+        <Footer />
+
+    </>
+  )
+}
+
+export default AppLayout
